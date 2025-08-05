c546ce02fbe6cd9784efb16d1f06e968999b200c
Diya Patel <pateldiya1005@gmail.com>
2025-08-05 15:36:32 +0530

    sevaaifinal.py

--- sevaaifinal.py
+++ sevaaifinal.py
@@ -72,6 +72,8 @@ def run_sevaai():
     response = detect_symptom(user_text)
     emotion = detect_emotion(user_text)
     print(f"DEBUG: Symptom: {response}, Emotion: {emotion}")
+if __name__ == "__main__":
+    run_sevaai()
 
     full_response = ""
     if emotion:
@@ -79,4 +81,5 @@ def run_sevaai():
     full_response += response
 
     speak_response(full_response)
+
     log_interaction(user_text, full_response)
